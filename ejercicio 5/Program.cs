9e17a4b85cd9f8d9714be594d31e02043c6fe0e4
AllanAgR <106946946+AllanAgR@users.noreply.github.com>
2022-11-12 04:30:31 -0600

    Add files via upload

---
+++ ejercicio 5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,y;
+            string l;
+
+            Console.WriteLine("Ingrese la coordenada x");
+            l = Console.ReadLine();
+            x = int.Parse(l);
+
+            Console.WriteLine("Ingrese la coordenada y");
+            l = Console.ReadLine();
+            y = int.Parse(l);
+
+            if (x>0 && y >0)
+            {
+                Console.WriteLine("Se encuentra en el primer cuadrante");
+            }
+            else{
+
+                if (x<0 && y>0)
+                {
+                    Console.WriteLine("Se encuentra en el segundo cuadrante");
+                }
+                else{
+
+                    if (x<0 && y<0)
+                    {
+                        Console.WriteLine("Se encuentra en el tercer cuadrante");
+                    }
+                    else{
+                        Console.WriteLine("Se encuentra en el cuarto cuadrante");
+                    }
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ejercicio 6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float s, am, st;
+            int a;
+            string l;
+
+            Console.WriteLine("Ingrese el sueldo");
+            l = Console.ReadLine();
+            s = float.Parse(l);
+
+            Console.WriteLine("Ingrese la antiegüedad");
+            l = Console.ReadLine();
+            a = int.Parse(l);
+
+            if (s<500 && a>10)
+            {
+                am = s*0.20f;
+                st = s+am;
+
+                Console.WriteLine("Sueldo a pagar:" + st);
+            }
+            else
+            {
+                
+                if (s<500)
+                {
+                    am = s*0.05f;
+                    st = s+am;
+
+                Console.WriteLine("Sueldo a pagar: " + st);
+                }
+                else
+                {
+                    Console.WriteLine("Sueldo a pagar: " + s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
